1c619ed05735084c9ab463e6e258e1a1a26e8d6d
Andrea Michelle Olivas Rivera <andreamichelleolivasrivera@gmail.com>
2025-11-17 14:24:39 -0600

    rg

--- terminal_ui.h
+++ terminal_ui.h
@@ -1,6 +1,5 @@
 #ifndef TERMINAL_UI_H
 #define TERMINAL_UI_H
-
 #include "order_book.h"
 #include <string>
 
@@ -8,11 +7,9 @@ class TerminalUI {
 public:
     explicit TerminalUI(OrderBook& book);
     void render(const std::string& symbol);
-
 private:
     OrderBook& book_;
     void clearScreen();
 };
-
 #endif
 
